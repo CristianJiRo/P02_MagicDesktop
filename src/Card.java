cf0ee90324cee41f9c606c2f080ee9ebeb8c2711
Cristian Jimenez <38853841x@iespoblenou.org>
2016-11-19 20:53:40 +0100

    Creamos el poho para las cartas.

---
+++ src/Card.java
@@ -0,0 +1,69 @@
+/**
+ * Created by Kamelot on 19/11/2016.
+ */
+public class Card {
+
+    private String id;
+    private String name;
+    private String text;
+    private String rarity;
+    private String urlImage;
+    private String type;
+    private String [] colors;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getRarity() {
+        return rarity;
+    }
+
+    public void setRarity(String rarity) {
+        this.rarity = rarity;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String[] getColors() {
+        return colors;
+    }
+
+    public void setColors(String[] colors) {
+        this.colors = colors;
+    }
+}
